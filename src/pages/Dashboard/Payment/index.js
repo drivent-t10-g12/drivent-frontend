e010af119957372e672bcdcb4c3ea9d128d375fd
elkurud <samukama18@gmail.com>
2023-08-02 16:05:48 -0300

    feat: non functional Payment screen

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,52 @@
+
+import styled from 'styled-components';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import { useState, useEffect } from 'react';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { enrollment } = useEnrollment();
+  const [ data, setData] = useState(<></>);
+
+  useEffect(() => {
+    console.log(enrollment);
+  }, [enrollment]);
+
+  return (
+    <>
+      <Header>
+          Ingresso e pagamento
+      </Header>
+      {!enrollment ? (
+        <>
+          <Message>
+            Você precisa completar sua inscrição antes<br/> de prosseguir pra escolha de ingresso
+          </Message>
+        </>
+      ) : ''}
+    </>
+  );
 }
+
+const Message = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 93%;
+  color: #8E8E8E;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const Header = styled.div`
+  color: #000;
+  font-family: Roboto;
+  font-size: 34px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;

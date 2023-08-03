2217b4e12314e1eded69969462076314b223c780
LsB.Dev <prof.luifel@gmail.com>
2023-08-03 11:08:35 -0300

    feat: layout de escolha do ticket pronto

---
+++ src/pages/Dashboard/Payment/CreditCard.js
@@ -0,0 +1,5 @@
+export default function CreditCard() {
+  return (
+    alert('Layout sem funcionalidade')
+  );
+};
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,16 +1,20 @@
-
 import styled from 'styled-components';
 import useEnrollment from '../../../hooks/api/useEnrollment';
 import { useState, useEffect } from 'react';
-// import axios from 'axios';
+import useToken from '../../../hooks/useToken';
+import axios from 'axios';
+import CreditCard from './CreditCard';
 
 export default function Payment() {
   const { enrollment } = useEnrollment();
   const [ presencialSelect, setPresencialSelect] = useState(false);
   const [ onlineSelect, setOnlineSelect] = useState(false);
+  const [withHotel, setWithHotel] = useState(false);
+  const [withoutHotel, setWithoutHotel] = useState(false);
+  const [tickets, setTickets] = useState([]);
+  const token = useToken();
 
-  console.log(onlineSelect);
-  console.log(presencialSelect);
+  console.log(tickets);
 
   function handleModality(modalidade) {
     if (modalidade === 'Presencial') {
@@ -19,17 +23,26 @@ export default function Payment() {
     } else if (modalidade === 'Online') {
       setPresencialSelect(false);
       setOnlineSelect(true);
-    }
+    };
+  };
+  function handleAccommodation(hospedagem) {
+    if(hospedagem === 'Sem Hotel') {
+      setWithoutHotel(true);
+      setWithHotel(false);
+    } else if (hospedagem === 'Com Hotel') {
+      setWithoutHotel(false);
+      setWithHotel(true);
+    };
   };
 
-  // useEffect(() => {
-  //   const config = {
-  //     header: {
-  //       Authorization: {}
-  //     }
-  //   }
-  //   axios.get()
-  // }, [enrollment]);
+  useEffect(() => {
+    const config = {
+      headers: { authorization: `Bearer ${token}` }
+    };
+    axios.get(`${process.env.REACT_APP_API_BASE_URL}/tickets/types`, config)
+      .then((res) => setTickets(res.data))
+      .catch((err) => console.log(err.responde.data));
+  }, [token]);
 
   return (
     <>
@@ -55,6 +68,26 @@ export default function Payment() {
               <Price>R$ 100</Price>
             </Modality>
           </Container>
+          { presencialSelect ? 
+            <>
+              <Instruction>Ótimo! Agora escolha sua modalidade de hospedagem</Instruction>
+              <Container>
+                <Hotel onClick={() => handleAccommodation('Sem Hotel')} color={withoutHotel}>
+                  <Mode>Sem Hotel</Mode>
+                  <Price>+ R$ 0</Price>
+                </Hotel>
+                <Hotel onClick={() => handleAccommodation('Com Hotel')} color={withHotel}>
+                  <Mode>Com Hotel</Mode>
+                  <Price>+ R$ 350</Price>
+                </Hotel>
+              </Container>          
+              <Instruction>Fechado! O total ficou em <p> {withHotel? 'R$ 600' : 'R$ 250'}</p>. Agora é só confirmar:</Instruction>
+              <Button onClick={() => CreditCard()}><p>RESERVAR INGRESSO</p></Button>
+            </> : <>
+              <Instruction>Fechado! O total ficou em <p> R$ 100</p>. Agora é só confirmar:</Instruction>
+              <Button><p>RESERVAR INGRESSO</p></Button> 
+            </>
+          }          
         </>
       }
     </>
@@ -80,12 +113,16 @@ const Instruction = styled.div`
   width: 100%;
   color: #8E8E8E;
   text-align: center;
-  margin-top: 40px;
+  margin-top: 25px;
   font-family: 'Roboto';
   font-size: 20px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+  p {
+    font-weight: bold;
+    margin-left: 6px;
+  }
 `;
 const Header = styled.div`
   color: #000;
@@ -129,4 +166,37 @@ const Modality = styled.div`
   border-radius: 20px;
   border: 1px solid #CECECE;
   background-color: ${(props) => props.color === true? ' #FFEED2' : 'white'};
+`;
+const Hotel = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 145px;
+  height: 145px;
+  border-radius: 20px;
+  border: 1px solid #CECECE;
+  background-color: ${(props) => props.color === true? ' #FFEED2' : 'white'};
+`;
+
+const Button = styled.button`
+  width: 162px;
+  height: 37px;
+  padding: 10px;
+  margin-top: 20px;
+  border-radius: 4px;
+  background: #E0E0E0;
+  border: none;
+  :hover {
+    cursor: pointer;
+  }
+  p {
+    color: #000;
+    text-align: center;
+    font-family: Roboto;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }  
 `;

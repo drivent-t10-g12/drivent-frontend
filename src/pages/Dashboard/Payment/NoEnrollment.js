4e552b2cc13ece6c71b6f64ed083b1f99173166a
LsB.Dev <prof.luifel@gmail.com>
2023-08-14 12:08:47 -0300

    feat: comp Cartão de Crédito e Ingresso

---
+++ src/pages/Dashboard/Payment/NoEnrollment.js
@@ -0,0 +1,11 @@
+import { Message } from './styled';
+
+export default function NoEnrollment() {
+  return (
+    <>
+      <Message>
+        Você precisa completar sua inscrição antes<br/> de prosseguir pra escolha de ingresso
+      </Message>
+    </>
+  );
+};
